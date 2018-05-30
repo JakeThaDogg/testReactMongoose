4487bf4a55c8bf00fa576292a5b6333023e26cbf
Gregolouise <gregolouise@gmail.com>
2018-05-30 13:47:31 +0200

    creation of user.js and content.js

---
+++ back/models/content.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose')
+
+var ContentSchema = new mongoose.Schema({
+  title: {
+    type: String
+  },
+  technical_name: {
+    type: String,
+    unique: true
+  },
+  short_image: {
+    type: String
+  },
+  short_description: {
+    type: String
+  },
+  main_description: {
+    type: String
+  },
+  type: {
+    type: String
+  },
+  author_id: {
+    type: String
+  },
+  publication: {
+    start_date: {type: Date},
+    end_date: {type: Date},
+    status: {type: String},
+    cause_of_refusal: {type: String}
+  },
+  challenge: {
+    end_of_participation_date: {type: Date},
+    synthesis: {type: String},
+    participations_to_show: {type: [String]}
+  },
+  categories: {
+    type: [String]
+  },
+  premium: {
+    type: Boolean
+  },
+  updated_by_administrator: {
+    type: Boolean
+  },
+  created_date: {
+    type: Date
+  },
+  last_update_date: {
+    type: Date
+  }
+})
+
+var Content = mongoose.model('Content', ContentSchema)
+module.exports = Content
