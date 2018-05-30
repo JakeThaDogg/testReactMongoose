8ba41e852f108499a7f4aae68ca15d3a6830c969
nezzar <kefifnezzar@gmai.com>
2018-05-30 11:26:08 +0200

    app.js modif

---
+++ back/app.js
@@ -0,0 +1,55 @@
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
+let mongoose = require('mongoose')
+let session = require('express-session')
+let MongoStore = require('connect-mongo')(session)
+
+//connection to MongoDB
+mongoose.connect('mongodb://localhost/testForAuth')
+var db = mongoose.connection
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  // we're connected!
+})
+
+//Use sessions for trackiing logins
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db
+    })
+  }));
+
+// parse incoming requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// serve static files from template
+app.use(express.static(__dirname + '/templateLogReg'));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+  
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
+  
+//listen on port 3000
+app.listen(3000, function () {
+    console.log('Express app listening on port 3000')
+  })
